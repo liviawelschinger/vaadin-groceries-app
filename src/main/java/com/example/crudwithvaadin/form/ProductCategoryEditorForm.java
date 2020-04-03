caba539a823260c98e33a33d0ddf85fde913e32f
liviawelschinger <livia-welschinger@t-online.de>
2020-04-03 14:08:51 +0200

    Filter category by name

--- src/main/java/com/example/crudwithvaadin/form/ProductCategoryEditorForm.java
+++ src/main/java/com/example/crudwithvaadin/form/ProductCategoryEditorForm.java
@@ -12,6 +12,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.beans.EventHandler;
 
@@ -19,113 +20,95 @@ import java.beans.EventHandler;
 @SpringComponent
 // Spring-Vaadin integration, same as Spring @Component; Attention: Vaadin has own annotation @Component
 @UIScope // that spring beans of this class are created in the access area and with the lifespan of a Vaadin UI session.
-public class ProductCategoryEditorForm extends VerticalLayout implements KeyNotifier {  // KeyNotifier: interface for components that support adding key event listeners to the their root elements
+public class ProductCategoryEditorForm extends VerticalLayout implements KeyNotifier {
 
-    private final ProductCategoryRepository categoryRepository;
-    // Field to edit property in ProductCategory entity
-    private TextField categoryName = new TextField("Product category name");
+    private final ProductCategoryRepository repository;
 
-    // Action buttons
-    private Button save = new Button("Save", VaadinIcon.CHECK.create());
-    private Button cancel = new Button("Cancel");
-    private Button delete = new Button("Delete", VaadinIcon.TRASH.create());
-    private HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
-    /*
-    Connects one or more {@code Field} components to properties of a backing data
-    type such as a bean type. With a binder, input components can be grouped
-    together into forms to easily create and update business objects with little
-    explicit logic needed to move data between the UI and the data layers of the
-    application.
+    /**
+     * The currently edited product category
      */
+    private ProductCategory category;
+
+    /* Fields to edit properties in ProductCategory entity */
+    TextField name = new TextField("Category name");
+
+    /* Action buttons */
+    Button save = new Button("Save", VaadinIcon.CHECK.create());
+    Button cancel = new Button("Cancel");
+    Button delete = new Button("Delete", VaadinIcon.TRASH.create());
+    HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
     Binder<ProductCategory> binder = new Binder<>(ProductCategory.class);
     private ChangeHandler changeHandler;
 
-    // Currently edited category
-    private ProductCategory category;
+    @Autowired
+    public ProductCategoryEditorForm(ProductCategoryRepository repository) {
+        this.repository = repository;
 
-    public ProductCategoryEditorForm(ProductCategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+        add(name, actions);
 
-        add(categoryName, actions);
-
-        // binds member fields found in the given objects
+        // bind using naming convention
         binder.bindInstanceFields(this);
 
-        // configure and style components
+        // Configure and style components
         setSpacing(true);
 
         save.getElement().getThemeList().add("primary");
-        cancel.getElement().getThemeList().add("secondary");
         delete.getElement().getThemeList().add("error");
 
-        addKeyPressListener(Key.ENTER, event -> save());
+        addKeyPressListener(Key.ENTER, e -> save());
 
-        save.addClickListener(event -> save());
-        cancel.addClickListener(event -> editCategory(category));
-        delete.addClickListener(event -> delete());
+        // wire action buttons to save, delete and reset
+        save.addClickListener(e -> save());
+        delete.addClickListener(e -> delete());
+        cancel.addClickListener(e -> editProductCategory(category));
         setVisible(false);
     }
 
-    /**
-     * Saves a product category
-     */
-    private void save() {
-        categoryRepository.save(category);
-        changeHandler.onChange();
-
-    }
-
-    /**
-     * Deletes a product category
-     */
-    private void  delete() {
-        categoryRepository.delete(category);
+    void delete() {
+        repository.delete(category);
         changeHandler.onChange();
     }
 
-    public void editCategory(ProductCategory c) {
-
-        if (c == null) {
-            // Invisible components don't receive any updates from the client-side
-            setVisible(false); // Sets the component visibility value.
-            // TODO Use?
-            return;
-        }
-
-        final boolean persisted = category.getId() != null;
-
-        if(persisted) {
-            category = categoryRepository.findById(c.getId()).get();
-        } else {
-            category = c;
-        }
-
-        /*
-            When category is persisted display cancel option in editor
-            When category is not persisted don't display cancel option in editor
-         */
-        cancel.setVisible(persisted);
-
-        // Bind customer properties to similarly named fields
-        // Could also use annotation or "manual binding" or programmatically
-        // moving values from fields to entities before saving
-        binder.setBean(category);
-
-        setVisible(true);
-
-        // Focus category name initially
-        categoryName.focus();
-
-    }
-
-    public void setChangeHandler(ChangeHandler handler) {
-        // ChangeHandler is notified when either save or delete is clicked
-        changeHandler = handler;
+    void save() {
+        repository.save(category);
+        changeHandler.onChange();
     }
 
     public interface ChangeHandler {
         void onChange();
     }
 
+    public final void editProductCategory(ProductCategory c) {
+        if (c == null) {
+            setVisible(false);
+            return;
+        }
+        final boolean persisted = c.getId() != null;
+        if (persisted) {
+            // Find fresh entity for editing
+            category = repository.findByName(c.getName());
+        }
+        else {
+            category = c;
+        }
+        cancel.setVisible(persisted);
+
+        // Bind category properties to similarly named fields
+        // Could also use annotation or "manual binding" or programmatically
+        // moving values from fields to entities before saving
+        binder.setBean(category);
+
+        setVisible(true);
+
+        // Focus first name initially
+        name.focus();
+    }
+
+    public void setChangeHandler(ChangeHandler h) {
+        // ChangeHandler is notified when either save or delete
+        // is clicked
+        changeHandler = h;
+    }
 
 }
--- src/main/java/com/example/crudwithvaadin/view/MainView.java
+++ src/main/java/com/example/crudwithvaadin/view/MainView.java
@@ -1,15 +1,19 @@
 package com.example.crudwithvaadin.view;
 
 import com.example.crudwithvaadin.entity.ProductCategory;
+import com.example.crudwithvaadin.form.ProductCategoryEditorForm;
 import com.example.crudwithvaadin.repository.ProductCategoryRepository;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.springframework.util.StringUtils;
+import com.vaadin.flow.component.button.Button;
 
+import java.awt.*;
 
 
 /**
@@ -21,25 +25,37 @@ public class MainView extends VerticalLayout {
     private final ProductCategoryRepository categoryRepository;
     private final Grid<ProductCategory> categoryGrid;
     private final TextField filter;
-    private Binder<ProductCategory> binder = new Binder<>(ProductCategory.class);
-
+    private final  Button addNewBtn;
+    private final ProductCategoryEditorForm editor;
 
     /**
      * Constructor
      * @param categoryRepository ProductCategoryRepository
      */
-    public MainView(ProductCategoryRepository categoryRepository) {
-
+    public MainView(ProductCategoryRepository categoryRepository, ProductCategoryEditorForm editor) {
 
         this.categoryRepository = categoryRepository;
+        this.editor = editor;
+
         this.filter = new TextField();
         filter.setPlaceholder("Filter category by name");
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(event -> {
             listProductCategories(event.getValue());
         });
+        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+
         this.categoryGrid = new Grid<>(ProductCategory.class); // Grid bound to the ProductCategory list
-        add(filter, categoryGrid);
+        categoryGrid.setHeight("300px");
+        categoryGrid.setColumns("id", "name");
+        categoryGrid.getColumnByKey("id").setWidth("80px").setFlexGrow(0);
+        categoryGrid.asSingleSelect().addValueChangeListener(event -> {
+            editor.editProductCategory(event.getValue());
+                });
+
+
+        add(actions, categoryGrid);
 
         // Initialize listing
         listProductCategories(null);
