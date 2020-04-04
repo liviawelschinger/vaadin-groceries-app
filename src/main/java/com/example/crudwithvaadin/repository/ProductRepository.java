27e6c153fb68bb66bada31020f2d14d072dab6e1
liviawelschinger <livia-welschinger@t-online.de>
2020-04-04 16:17:38 +0200

    Add findbyName method to ProductRepository

--- src/main/java/com/example/crudwithvaadin/repository/ProductRepository.java
+++ src/main/java/com/example/crudwithvaadin/repository/ProductRepository.java
@@ -2,9 +2,16 @@ package com.example.crudwithvaadin.repository;
 
 import com.example.crudwithvaadin.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for Product
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    String QUERY_BY_NAME = "SELECT p FROM Product p WHERE p.name LIKE :name" + "%";
+
+    @Query(QUERY_BY_NAME)
+    Product findByName(@Param("name") String name);
 }
