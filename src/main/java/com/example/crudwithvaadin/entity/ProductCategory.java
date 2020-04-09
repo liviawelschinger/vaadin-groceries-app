ba5ea4fdb5b04591431b0bad52d2d488abe041b3
liviawelschinger <livia-welschinger@t-online.de>
2020-04-09 17:17:37 +0200

    Fixed mapping for class ProductCategory, remove Lombok

--- src/main/java/com/example/crudwithvaadin/entity/ProductCategory.java
+++ src/main/java/com/example/crudwithvaadin/entity/ProductCategory.java
@@ -1,44 +1,69 @@
 package com.example.crudwithvaadin.entity;
 
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * Entity class for ProductCategory
  * @author Livia Welschinger
  */
-@Data
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name = "category")
+@Table(name = "category") // name of SQL table
 public class ProductCategory {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // use Long for later examiniation (null (not persited category) or not null (persisted category)) The Long class wraps a value of the primitive type long in an object. An object of type Long contains a single field whose type is long.
 
     @Column(name = "name")
     private String name;
 
-    @Column(name = "local_date")
-    private LocalDate localDate;
+    @Column(name = "date_processed")
+    private LocalDate dateProcessed;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "productCategory")
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "productCategory") // mapped by Java entitity, must be same name as Java Object in Product class therefore productCategory
     private List<Product> productList = new ArrayList<>();
 
-    public ProductCategory(String name, LocalDate localDate) {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ProductCategory() {
+
+    }
+
+    public ProductCategory(String name, LocalDate dateProcessed) {
         this.name = name;
-        this.localDate = localDate;
+        this.dateProcessed = dateProcessed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateProcessed() {
+        return dateProcessed;
+    }
+
+    public void setDateProcessed(LocalDate dateProcessed) {
+        this.dateProcessed = dateProcessed;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
     }
 }
