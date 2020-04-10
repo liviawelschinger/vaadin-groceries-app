476bcf32c1423997ec33a8bb37f0b7947651af1b
liviawelschinger <livia-welschinger@t-online.de>
2020-04-10 17:06:44 +0200

    Add sample product Apple in category Fruits

--- src/main/java/com/example/crudwithvaadin/CrudWithVaadinApplication.java
+++ src/main/java/com/example/crudwithvaadin/CrudWithVaadinApplication.java
@@ -1,8 +1,11 @@
 package com.example.crudwithvaadin;
 
+import com.example.crudwithvaadin.entity.Product;
 import com.example.crudwithvaadin.entity.ProductCategory;
 import com.example.crudwithvaadin.repository.ProductCategoryRepository;
 import java.time.LocalDate;
+
+import com.example.crudwithvaadin.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -33,7 +36,7 @@ public class CrudWithVaadinApplication {
 	 * @return args
 	 */
 	@Bean
-	public CommandLineRunner loadData(ProductCategoryRepository categoryRepository) {
+	public CommandLineRunner loadData(ProductCategoryRepository categoryRepository, ProductRepository productRepository) {
 		return (args) -> {
 			// save a couple of product categories
 			LocalDate now = LocalDate.now();
@@ -51,6 +54,8 @@ public class CrudWithVaadinApplication {
 			}
 
 			LOGGER.info("Found category by name: " + categoryRepository.findByName("Fruits").getName());
+
+			productRepository.save(new Product("Apple", 1.45, now, categoryRepository.findByName("Fruits"), categoryRepository.findByName("Fruits").getName()));
 		};
 	}
 }
